6e801d3cb2df2f1d59eacce1c8d123c59ba04316
pmoros <pmoros@outlook.com>
2022-09-11 18:52:12 -0500

    feat: proyect java layout

---
+++ src/main/java/com/ambrosia/inscriptions/InscriptionsApplication.java
@@ -0,0 +1,13 @@
+package com.ambrosia.inscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscriptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambrosia/inscriptions/InscriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambrosia.inscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
