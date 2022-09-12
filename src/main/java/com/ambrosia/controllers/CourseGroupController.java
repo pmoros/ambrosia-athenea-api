30db267f2feb165a5ab3874e27c5a962fc4faa2d
pmoros <pmoros@outlook.com>
2022-09-12 16:40:52 -0500

    refactor: http status included

--- src/main/java/com/ambrosia/controllers/CourseGroupController.java
+++ src/main/java/com/ambrosia/controllers/CourseGroupController.java
@@ -3,9 +3,11 @@ package com.ambrosia.controllers;
 import com.ambrosia.models.CourseGroup;
 import com.ambrosia.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +17,10 @@ public class CourseGroupController {
     private CourseGroupRepository courseGroupRepository;
 
     @PostMapping("/inscriptions/course-group")
-    public String createCourseGroup(@RequestBody CourseGroup courseGroup) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createCourseGroup(@RequestBody CourseGroup courseGroup) {
+        // TODO: check if courseGroup already exists
         this.courseGroupRepository.save(courseGroup);
-        return "CourseGroup created";
     }
 
     // enpoint to get all course groups
