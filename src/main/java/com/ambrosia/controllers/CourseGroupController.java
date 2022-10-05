8d5be530374237a44378768c30a12af1c432632a
pmoros <pmoros@outlook.com>
2022-10-05 00:42:55 -0500

    feat: get course by id

--- src/main/java/com/ambrosia/controllers/CourseGroupController.java
+++ src/main/java/com/ambrosia/controllers/CourseGroupController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,12 +29,17 @@ public class CourseGroupController {
 
     Logger logger = LoggerFactory.getLogger(CourseGroupController.class);
 
-    @GetMapping("")
+    @GetMapping("/")
     public Iterable<CourseGroup> getCourseGroups() {
         return courseGroupRepository.findAll();
     }
 
-    @PostMapping("")
+    @GetMapping("/{CourseGroupId}")
+    public CourseGroup getCourseGroup(@PathVariable("CourseGroupId") Long CourseGroupId) {
+        return courseGroupRepository.findById(CourseGroupId).get();
+    }
+
+    @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public CourseGroup createCourseGroup(@RequestBody CourseGroup courseGroup) {
         courseGroup = this.courseGroupRepository.save(courseGroup);
