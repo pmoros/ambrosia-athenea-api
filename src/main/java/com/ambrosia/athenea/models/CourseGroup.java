726049cf3d6ab150f97b2cf329cf2408a3890c44
pmoros <pmoros@outlook.com>
2022-10-29 17:54:19 -0500

    fix: infinite recursion searching course groups

--- src/main/java/com/ambrosia/athenea/models/CourseGroup.java
+++ src/main/java/com/ambrosia/athenea/models/CourseGroup.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.Getter;
@@ -47,5 +48,6 @@ public class CourseGroup implements Serializable {
     private List<Schedule> schedules;
 
     @ManyToMany(mappedBy = "courseGroups")
+    @JsonIgnore
     private List<AcademicHistory> academicHistories;
 }
